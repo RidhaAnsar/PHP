7126dea8d19ad3c7a84ff4bc0aa77298fd7a3bbf
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-23 21:34:21 +0530

    length.php

---
+++ length.php
@@ -0,0 +1,11 @@
+<?php
+function getStringLength($string) {
+    return strlen($string);
+}
+
+// Example usage
+$input = "hello world!";
+$output = getStringLength($input);
+echo "String: $input\n";
+echo "Length: $output\n";
+?>
