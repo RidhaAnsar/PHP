c4330543d366a8275780afa9c2ac7278f8b4b69c
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-23 21:32:27 +0530

    upper.php

---
+++ upper.php
@@ -0,0 +1,11 @@
+<?php
+function convertToUpperCase($string) {
+    return strtoupper($string);
+}
+
+// Example usage
+$input = "hello world!";
+$output = convertToUpperCase($input);
+echo "Original: $input\n";
+echo "Upper Case: $output\n";
+?>
